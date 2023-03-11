653c65fb70a41182c158900a651a60261e2cb8d4
Asma-A-S <asma.benzerrouk@gmail.com>
2023-03-12 00:00:47 +0100

    ajout condition pour modifier image banner selon la page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../styles/banner.css'
-function Banner({ image, texte }) {
-  return (
-    <div className="banner">
-      <img className="banner_img" src={image} alt="bannière" />
-      {texte ? <h1 className="banner_texte">{texte}</h1> : null}
-    </div>
-  )
+function Banner({ origin, texte }) {
+  if (origin === 'Home') {
+    return (
+      <div className="banner banner_home">
+        {<h1 className="banner_texte">{texte}</h1>}
+      </div>
+    )
+  }
+  if (origin === 'About') {
+    return <div className="banner banner_about"></div>
+  } else {
+    return null
+  }
 }
 export default Banner
+//<img className="banner_img" src={image} alt="bannière" />
